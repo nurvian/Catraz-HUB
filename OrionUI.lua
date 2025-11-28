3d6ddc7d02f1ec22c0a8b6df78d4758be8b542b4
Fachry <nurvian67@gmail.com>
2025-11-29 00:06:49 +0700

    add orion ui

---
+++ OrionUI.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 6701277882 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Catraz Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Catraz"})
+
+
+
+
+
+end
+OrionLib:Init()
