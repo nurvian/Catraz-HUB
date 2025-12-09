5ebac89edff8ba2bbf0af6fb2f669c52078aea1e
Fachry <nurvian67@gmail.com>
2025-12-09 23:12:01 +0700

    feat : auto detect map

--- Catraz/main.lua
+++ Catraz/main.lua
@@ -1,6 +1,7 @@
 local games = {
     [121864768012064] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/FishIt.lua"
-    [76558904092080] = "https://https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua"
+    [76558904092080] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua?token=GHSAT0AAAAAADQHKTHGQA4LM7BZ3BTJNJE62JYJH3Q"
+    [129009554587176] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua?token=GHSAT0AAAAAADQHKTHGQA4LM7BZ3BTJNJE62JYJH3Q"
 }
 
 local currentID = game.PlaceId
