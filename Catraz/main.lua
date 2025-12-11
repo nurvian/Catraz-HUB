a3ebb0fbc51d106e6d19dfb9874cef36a3668fdb
Fachry <nurvian67@gmail.com>
2025-12-12 00:36:56 +0700

    The Forge | feat : ready beta test release 4

--- Catraz/main.lua
+++ Catraz/main.lua
@@ -8,7 +8,7 @@ local currentID = game.PlaceId
 local scriptURL = games[currentID]
 
 if scriptURL then
-    loadstring(game:HttpGet(scriptURL))()
+    loadstring(game:HttpGet('scriptURL'))()
 else
     game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
 end
