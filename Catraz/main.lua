8a31fa269b63093d32414770648a2a83b66c5e62
Fachry <nurvian67@gmail.com>
2025-12-12 18:40:01 +0700

    The Forge | fix : load issue

--- Catraz/main.lua
+++ Catraz/main.lua
@@ -1,17 +1,16 @@
 local games = {
-    [121864768012064] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/FishIt.lua", -- Tambah koma
-    [76558904092080]  = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua", -- Tambah koma
+    [121864768012064] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/FishIt.lua", 
+    [76558904092080]  = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua", 
     [129009554587176] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/TheForge.lua"
 }
 
 local currentID = game.PlaceId
 local scriptURL = games[currentID]
 
--- Debugging (Supaya kamu tau ID game yang kamu mainkan sekarang berapa)
+-- Debugging 
 print("Current Game ID:", currentID) 
 
 if scriptURL then
-    -- Perbaikan: Hapus tanda kutip (' ') di dalam kurung HttpGet
     loadstring(game:HttpGet(scriptURL))() 
 else
     game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
