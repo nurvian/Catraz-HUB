a5997bc6e079752f5685d0b21da91d416767d337
Fachry <nurvian67@gmail.com>
2026-01-08 21:35:12 +0700

    Testing

--- Catraz/main.lua
+++ Catraz/main.lua
@@ -1,19 +1,17 @@
 local games = {
-    [76558904092080]  = "https://api.junkie-development.de/api/v1/luascripts/public/ea8ae5d36a20a896bb7f85ea42d1378ea6925566a1ddfb715928d016ad3fe70e/download", 
-    [129009554587176] = "https://api.junkie-development.de/api/v1/luascripts/public/ea8ae5d36a20a896bb7f85ea42d1378ea6925566a1ddfb715928d016ad3fe70e/download",
-    [131884594917121] = "https://api.junkie-development.de/api/v1/luascripts/public/ea8ae5d36a20a896bb7f85ea42d1378ea6925566a1ddfb715928d016ad3fe70e/download",
+    [121864768012064] = "https://raw.githubusercontent.com/nurvian/Catraz-HUB/refs/heads/main/Catraz/games/FishIt.lua", 
+    [76558904092080]  = "https://api.luarmor.net/files/v4/loaders/df7f1ca84a4dcd17d4043c7f2330888a.lua", 
+    [129009554587176] = "https://api.luarmor.net/files/v4/loaders/df7f1ca84a4dcd17d4043c7f2330888a.lua",
+    [131884594917121] = "https://api.luarmor.net/files/v4/loaders/df7f1ca84a4dcd17d4043c7f2330888a.lua",
 }
 
 local currentID = game.PlaceId
 local scriptURL = games[currentID]
 
 -- Debugging 
-print("[Catraz Hub] Current Game ID:", currentID) 
+print("Current Game ID:", currentID) 
 
 if scriptURL then
-
-    getgenv().SCRIPT_KEY = "KEYLESS" 
-    
     loadstring(game:HttpGet(scriptURL))() 
 else
     game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
